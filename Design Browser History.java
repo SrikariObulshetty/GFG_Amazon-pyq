3c6b573fcd78af20ae592fd7e38d8f6d0cba7c61
Srikari Obulshetty <srikariobulshetty@gmail.com>
2025-01-17 23:39:44 +0530

    Create  Design Browser History.java

---
+++ Design Browser History.java
@@ -0,0 +1,49 @@
+class BrowserHistory {
+    class Node{
+        String  url;
+        Node prev;
+        Node next;
+        Node(String url){
+            this.url=url;
+            prev=null;
+            next=null;
+        }
+    }
+    Node curr;
+    public BrowserHistory(String homepage) {
+          curr=new Node(homepage);
+
+    }
+    
+    public void visit(String url) {
+        Node newNode =new Node(url);
+        //attach to curr node , be careful
+        newNode.prev=curr;
+        curr.next=newNode;
+        curr=newNode;
+    }
+    
+    public String back(int steps) {
+        while(steps>0 && curr.prev!=null){
+             curr=curr.prev;
+             steps--;
+        }
+        return curr.url;
+    }
+    
+    public String forward(int steps) {
+        while(steps>0 && curr.next!=null){
+             curr=curr.next;
+             steps--;
+        }
+        return curr.url;
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
