9f45cf134110f4676a54b87deed87d3cd6ef93f5
Srikari Obulshetty <srikariobulshetty@gmail.com>
2025-01-18 07:08:32 +0530

    Create Reverse a linked list.java

---
+++ Reverse a linked list.java
@@ -0,0 +1,29 @@
+
+class Node {
+    int data;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+        if(head==null || head.next==null){
+            return head; // no need to reverse
+        }
+        Node curr=head;
+        Node prev=null;
+        Node next=null;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
