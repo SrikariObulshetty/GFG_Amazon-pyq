292ba1034de0655930cd0850c39ea96eb5bef27e
Srikari Obulshetty <srikariobulshetty@gmail.com>
2025-01-25 19:36:05 +0530

    Create Find the first node of loop in linked list.java

---
+++ Find the first node of loop in linked list.java
@@ -0,0 +1,38 @@
+/* class Node
+{
+    int data;
+    Node next;
+
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public static Node findFirstNode(Node head) {
+        // code here
+         if(head==null || head.next==null){
+              return null;
+         }
+         Node slow=head;
+         Node fast=head;
+         while(fast!=null && fast.next!=null){
+              slow=slow.next;
+              fast=fast.next.next;
+              if(slow==fast){
+                  break;
+              }
+         }
+         if(fast==null || fast.next==null){
+               return null; // no loop
+         }
+         slow=head;
+         while(slow!=fast){
+             slow=slow.next;
+             fast=fast.next;
+         }
+         return slow;
+    }
+}
